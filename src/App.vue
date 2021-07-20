242ad330acfc884c2766292319ee741354a93156
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-20 23:56:38 +0300

    Initialize vite

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script setup>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top: 60px;
+}
+</style>
