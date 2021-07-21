edb03ed68af714fc6edc4a9f95b6cfd463ddef71
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-22 02:52:39 +0300

    Fix syntax mistakes

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,17 @@
-
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '../components/Home.vue';
 
 const routes = [
-    {
-        path: '/',
-        component: Home,
-    },
+  {
+    path: '/',
+    component: Home,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
