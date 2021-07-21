273ffadf8c76a1d6e304bb143cc32d2c676540cb
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-22 02:44:58 +0300

    Move router to its own file

--- src/main.js
+++ src/main.js
@@ -1,20 +1,10 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory} from 'vue-router';
 
 import App from './App.vue';
-import Home from './components/Home.vue';
+import router from './router';
 import './index.css';
 
-const routes = [
-    { path: '/', component: Home }
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
-
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(router);
 
