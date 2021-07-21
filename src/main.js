f833c5ebd9cb9d0a44280a0c3328693c47a8e698
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-22 02:06:02 +0300

    Add vue router and home page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory} from 'vue-router';
+
 import App from './App.vue';
+import Home from './components/Home.vue';
 import './index.css';
 
-createApp(App).mount('#app');
+const routes = [
+    { path: '/', component: Home }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app');
