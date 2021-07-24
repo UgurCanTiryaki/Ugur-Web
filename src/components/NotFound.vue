8a4dd6ae54d582b288b274d0696b106b59ef8243
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-24 18:53:44 +0300

    Refactor notfound component

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="h-screen bg-gray-100 flex flex-col justify-center items-center">
-    <p class="text-center font-mono">
-      <span class="text-9xl">404</span>
-      <br />
-      <span class="text-2xl">Page Not</span>
-      <br />
-      <span class="text-2xl">Found</span>
-      <br />
-      <br />
-      <i class="text-xl">
-        <router-link to="/">Click Here to Go to Home</router-link>
-      </i>
-    </p>
+  <div class="h-screen flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-center items-center mb-10 font-mono">
+      <div class="text-9xl">404</div>
+      <div class="text-2xl">Page Not</div>
+      <div class="text-2xl">Found</div>
+    </div>
+    <router-link to="/" class="text-xl font-mono italic">
+      Click Here to Go to Home
+    </router-link>
   </div>
 </template>
 
