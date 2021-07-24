552601c64a41122b5bbf1362e68471663068f7be
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-24 11:00:54 +0300

    Create not found component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-screen bg-gray-100 flex flex-col justify-center items-center">
+    <p class="text-center font-mono">
+      <span class="text-9xl">404</span>
+      <br />
+      <span class="text-2xl">Page Not</span>
+      <br />
+      <span class="text-2xl">Found</span>
+      <br />
+      <br />
+      <i class="text-xl">
+        <router-link to="/">Click Here to Go to Home</router-link>
+      </i>
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
