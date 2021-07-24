dc24596a028e55d09d91e59ec4d831aab6e8c57b
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-24 19:25:22 +0300

    Refactor font location to component level

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="h-screen flex flex-col justify-center items-center">
-    <div class="flex flex-col justify-center items-center mb-10 font-mono">
+  <div class="h-screen flex flex-col justify-center items-center font-mono">
+    <div class="flex flex-col justify-center items-center mb-10">
       <div class="text-9xl">404</div>
       <div class="text-3xl">Page Not</div>
       <div class="text-3xl">Found</div>
     </div>
-    <router-link to="/" class="text-xl font-mono italic">
+    <router-link to="/" class="text-xl italic">
       Click Here to Go to Home
     </router-link>
   </div>
