60e8d99e89dac8da5cf79a9f751f5f2437cbd3cd
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-24 19:05:34 +0300

    Change font size

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -2,8 +2,8 @@
   <div class="h-screen flex flex-col justify-center items-center">
     <div class="flex flex-col justify-center items-center mb-10 font-mono">
       <div class="text-9xl">404</div>
-      <div class="text-2xl">Page Not</div>
-      <div class="text-2xl">Found</div>
+      <div class="text-3xl">Page Not</div>
+      <div class="text-3xl">Found</div>
     </div>
     <router-link to="/" class="text-xl font-mono italic">
       Click Here to Go to Home
