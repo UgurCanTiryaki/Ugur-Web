10a8f4237f8f6ca4cc3436264bca441f8d8b1ef3
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-24 11:01:23 +0300

    Add not found route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '../components/Home.vue';
+import NotFound from '../components/NotFound.vue';
 
 const routes = [
   {
     path: '/',
     component: Home,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFound,
   },
 ];
 
