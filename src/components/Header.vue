688fec460e6ae62758c930d30d9794142005e07a
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-25 17:41:49 +0300

    Remove unnecessary spaces

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div class="h-32 bg-gray-900 opacity-90 flex justify-between items-center px-16 text-white">
     <span class="ml-40 text-5xl">🆄🅲🆃</span>
     <div class="flex flex-row justify-around text-xl">
-      <router-link :to="{ name: 'home' }" class="ml-4"> Home </router-link>
+      <router-link :to="{ name: 'home' }" class="ml-4">Home</router-link>
       <!-- Below router links' "to" values will be named routes. These are placeholder fow now. -->
       <router-link to="/about" class="ml-4">About</router-link>
       <router-link to="/contact" class="ml-4">Contact</router-link>
