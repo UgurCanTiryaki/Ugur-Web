8fdfa0a89ccfdf4a9df607f88294467b13bfb1c5
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-25 17:25:15 +0300

    Move text white to upper level

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="h-32 bg-gray-900 opacity-90 flex justify-between items-center px-16">
-    <span class="ml-40 text-white text-5xl">🆄🅲🆃</span>
-    <div class="flex flex-row justify-around text-xl text-white">
+  <div class="h-32 bg-gray-900 opacity-90 flex justify-between items-center px-16 text-white">
+    <span class="ml-40 text-5xl">🆄🅲🆃</span>
+    <div class="flex flex-row justify-around text-xl">
       <router-link :to="{ name: 'home' }" class="ml-4"> Home </router-link>
       <!-- Below router links' "to" values will be named routes. These are placeholder fow now. -->
       <router-link to="/about" class="ml-4">About</router-link>
