b7149938682cf472e236eaeb634385439a9a9ebd
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-07-29 11:21:06 +0300

    Add footer to app component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div class="relative">
     <Header class="absolute w-full"></Header>
     <router-view class="bg-gray-100"></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
-    Header
-  }
-}
+    Header,
+    Footer,
+  },
+};
 </script>
 
 <style>
