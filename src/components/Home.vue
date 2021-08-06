e83c5c078f471f12f331abbc2a3617fa7e2020c2
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-08-06 13:52:01 +0300

    Add todo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,6 @@
 <template>
+  <!--TODO: The p tag and h-screen are both temporary here.
+   There will be another tags which has a total height greater than header's height-->
   <p class="h-screen">
     Home
   </p>
