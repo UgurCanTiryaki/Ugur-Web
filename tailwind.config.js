bda975f2fbdbaf6cfacd6283274d32d0a7312720
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-08-08 13:13:11 +0300

    Add josefin slab font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        josefinSlab: ['Josefin Slab', 'serif'],
+      },
+    },
   },
   variants: {
     extend: {},
