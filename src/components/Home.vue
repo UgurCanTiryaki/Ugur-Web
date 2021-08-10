b8b7d9fdf358a274b8882a08a270e59e6e8651ec
UgurCanTiryaki <72738716+UgurCanTiryaki@users.noreply.github.com>
2021-08-10 10:47:27 +0300

    Refactor home component span
    The span tag's syntax in home page changed.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,8 +13,8 @@
   >
     <div class="flex flex-col justify-evenly items-center h-64">
       <span class="text-5xl">Hello, I am Ugur Can</span>
-      <span class="text-4xl"
-        >Backend Developer, experienced with ExpressJS and Golang
+      <span class="text-4xl">
+        Backend Developer, experienced with ExpressJS and Golang
       </span>
       <div class="flex justify-evenly w-1/2 h-10 text-xl">
         <HomeButton buttonLink="/about" buttonText="About" />
