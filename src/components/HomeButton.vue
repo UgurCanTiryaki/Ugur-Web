8cf42d9abc7923c64b8c851f971f926a552408bb
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-08-10 08:38:16 +0300

    Create home button component

---
+++ src/components/HomeButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <router-link
+    class="
+      flex
+      justify-center
+      items-center
+      w-3/12
+      rounded-3xl
+      bg-rose-600
+      hover:bg-rose-800
+    "
+    :to="buttonLink"
+  >
+    {{ buttonText }}
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonLink: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
